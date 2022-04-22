c455ff87deef274da78184a4ed1adbf6715997c1
ssandland <shae.sandland@wsu.edu>
2022-04-21 19:27:09 -0700

    2nd update to Classroom and Course

--- Cpts 322 Final Project/Classroom.js
+++ Cpts 322 Final Project/Classroom.js
@@ -4,23 +4,30 @@ import {LinkedList} from './LinkedList.js'
 import {Course} from './Course.js'
 
 class Classroom extends List, Course, Trends {
-
+    //initiate everything null or as new class objects
     constructor() {
         this.teacher = null;
-        this.courseList = null;
+        this.courseList = new LinkedList<Course>();
         this.name = 'default';
-        this.classrooomTrend = null;
+        this.classrooomTrend = new Trend();
+    }
+    //initiate variable with var inputs
+    constructor(newTeacher, newCourseList, newName, newClassroomTrend) {
+        this.teacher = newTeacher;
+        this.courseList = new LinkedList<Course>(newCourseList);
+        this.name = newName;
+        this.classrooomTrend = new Trend<Classroom>(newClassroomTrend);
     }
 
     SetName(newName) {
-        if(newName == null)
+        if(newName == null) //checking if the input var newName is null if so return
             return;
-        else if(typeof newName === 'string') {
+        else if(typeof newName === 'string') { //checking if the input var newName is string before assigning to name
             this.name = newName;
             return;   
         }
         else{
-            return;
+            return; //if the input is not null and is not type string
         }
     }
 
@@ -29,7 +36,10 @@ class Classroom extends List, Course, Trends {
     }
 
     SetName(newName) {
-        if (typeof newName === 'string')
+        if(newName == null){
+            return;
+        }
+        if (typeof newName === 'string') //checking if the input var newName is a string
             this.name = newName;
         return;
     }
@@ -44,13 +54,17 @@ class Classroom extends List, Course, Trends {
 
         //If the input var is a class Course type then append to the list of courses no need to assign the var in the class type
         //Checking if the data input is class object from Course
-        if(typeof newCourse === 'object')
+        if(typeof newCourse === 'object') {
             this.courseList.Append(newCourse);
+            return;
+        }
+        return;
+        
     }
 
     RemoveCourse(cName) {
         var nextHolder = this.courseList.GetFirst().next;
-        if(this.courseList.GetFirst().courseName == cName) {
+        if(this.courseList.GetFirst().courseName == cName) { //If the Course to be removed is the root node
             delete(root);
             root = nextHolder;
             return;
@@ -77,11 +91,13 @@ class Classroom extends List, Course, Trends {
     }
 
     GetClassroomTrend() {
-        return this.classrooomTrend = Trend.GetTable();
+        return this.classrooomTrend.GetTable();
     }
 
     UpdateClassroomTrend() {
         this.classrooomTrend = Trend.UpdateTable(this.courseList);
+        this.classrooomTrend = Trend.UpdateChart(this.courseList);
+        this.classrooomTrend = Trend.UpdateGraph(this.courseList);
         return;
     }
 }
--- Cpts 322 Final Project/Course.js
+++ Cpts 322 Final Project/Course.js
@@ -5,36 +5,70 @@ import LinkedList from './LinkedList.js'
 import RegisteredUser from './RegisteredUser.js'
 import {Teacher} from './Teacher.js'
 import {QuestionSet} from './QuestionSet.js'
+import {Trends} from './Trends.js'
 
 
-class Course extends LinkedList, RegisteredUser, Classroom, Teacher {
-
+class Course extends LinkedList, RegisteredUser, Classroom, Teacher, Trends {
+    //initiate vars to a default value or null and for those that are of class objects
     constructor() {
         this.courseName = 'defaultCourseName';
         this.teacher = null;
-        this.strudents = null;
+        this.strudents = new LinkedList<Student>();
         //List<this.Student> null;
-        this.courseTrends = null;
-        this.questionSet = List<QuestionSet>.;
+        this.courseTrends = new Trend<Course>();
+        this.questionSet = new LinkedList<QuestionSet>();
+    }
+    //initiate the vars based on variable input
+    constructor(newCourseName, newTeacher, newStudents, newCourseTrends, newQuestionSet) {
+        this.courseName = newCourseName;
+        this.teacher = newTeacher;
+        this.strudents = new LinkedList<Student>(newStudents);
+        //List<this.Student> null;
+        this.courseTrends = new Trend<Course>(newCourseTrends);
+        this.questionSet = new LinkedList<QuestionSet>(newQuestionSet);
     }
 
     TakeAttendance() {
-        if(this.courseName === null)
+        if(this.courseName === null) //if there is no course name then there arent students to take attendance for 
             return;
         Teacher.TakeAttendance(this.courseName);
         return;   
     }
 
-    MakeQuestionSet() {
-
+    MakeQuestionSet(numQuestions) {
+        //ask for how many questions they want to add
+        //enter a for loop for inputting questions into question set until reaching the last question
+        QuestionSet* newQSet;
+        this.questionSet.List<QuestionSet>.append(newQSet);
+        for(const i; i < numQuestions; i++) {
+            newQSet.AddQuestion();
+        }
+        return;
     }
 
     ModifyQuestionSet() {
-
+        //find question set that's going to be changed
+        for(const i = 0; i < currQuestionSet.Size(); i++) {
+            //get teacher input for the string of options to have a string input for SetOption(int, string)
+            currQuestionSet.questionList.SetOption(i, );
+            //get teacher input for a string of for the answer for SetAnswer(int, string)
+            currQuestionSet.questionList.SetAnswer(i, );
+            //get teacher input for a double value for points to use with SetPoints(int, double)
+            currQuestionSet.questionList.SetPoints(i, );
+                
+        } 
+        return;
     }
 
-    RemoveQuestionSet() {
-
+    RemoveQuestionSet(removeThisQSet) {
+        var prev = this.questionSet; //saving the location of the previous question to no lose data in the Linked list
+        while(prev.questionList.GetQuestion(0) != removeThisQSet.questionList.GetQuestion(0)) { //Checking if the first question of the current location in the list is the same as the first question of the set that we want to delete
+            if(prev.next == null)
+                return;
+            prev = prev.next;
+        }
+        prev.next = prev.next.next; //overwriting the the next of the prev to the 2nd node after itself to git rid of the questionset we want to remove
+        return;
     }
 
     GetCourseName() {
@@ -52,32 +86,41 @@ class Course extends LinkedList, RegisteredUser, Classroom, Teacher {
     }
 
     GetCourseTrend() {
-        
-        return this.courseTrends;
+        return this.courseTrends.GetGraph<Course>();
     }
 
-    GetQuestionSetTrend(qSet) {
+    GetQuestionSetTrend(qSetnum) {
         //have to check if the inputted question set qSet is in the local questionSet linkedList -> List<questionSet>
         //Call the QuestionSet function for Trend GetQuestiomSetTrend()
-        qSet.GetQuestionSetTrend()
-        return this.questionSet.List<qSet>.GetQuestionSetTrend(QuestionSet* questionList.GetQestionSet());
+        return this.questionSet.GetQuestionSetTrend(qSetnum); //trying to call GetQuestionSetTrend(int) from QuestionSet class
+        /*
+        var curr = new LinkedList<QuestionSet>(this.questionSet);
+        for(const i = 0; i < qSetnum; i++) {
+            curr = curr.next;
+        }
+        return curr.GetQuestionSetTrend(QuestionSet* questionList.GetQestionSet());
+        */
     }
 
-    
+    GetQuestionTrend(qSetNum, questionNum) {
+        return this.questionSet.GetQuestionTrend(qSetNum, questionNum); //Trying to call QuestionSet's function GetQuestionTrend(int, int)
+    }
 
-    /*
-        void TakeAttendence();
-        void MakeQuestionSet();
-        Void ModifyQuestionSet();
-        void RemoveQuestionSet();
-        string GetCourseName();
-        string GetTeacherName()
-        List<string> GetStudentName();
-        Trend GetCourseTrend();
-        Trend GetQuestionSetTrend(int);
-        Trend GetQuestionTrend(int, int);
-        void StartQuestionSet();
-        void EndQuestionSet();
-        void UpdateCourseTrend();
-    */
+    StartQuestionSet() {
+        this.questionSet.StartQuz();
+        return;
+    }
+
+    EndQuestionSet() {
+        this.questionSet.EndQuiz();
+        return;
+    }
+
+    UpdateCourseTrend() {
+        this.courseTrends.UpdateChart<QuestionSet>(this.courseTrends);
+        this.courseTrends.UpdateGraph<QuestionSet>(this.courseTrends);
+        this.courseTrends.UpdateTable<QuestionSet>(this.courseTrends);
+        return;
+    }
+
 }
