24bba1a7147335c9160813993620facb6da05bff
ssandland <shae.sandland@wsu.edu>
2022-04-21 18:06:38 -0700

    updated Classroom and Course

--- Cpts 322 Final Project/Classroom.js
+++ Cpts 322 Final Project/Classroom.js
@@ -1,4 +1,87 @@
+//Need to import Trend, Course, teacher?,
+import {Trend} from './Trend.js'
+import {LinkedList} from './LinkedList.js'
+import {Course} from './Course.js'
 
-class Classroom {
+class Classroom extends List, Course, Trends {
 
+    constructor() {
+        this.teacher = null;
+        this.courseList = null;
+        this.name = 'default';
+        this.classrooomTrend = null;
+    }
+
+    SetName(newName) {
+        if(newName == null)
+            return;
+        else if(typeof newName === 'string') {
+            this.name = newName;
+            return;   
+        }
+        else{
+            return;
+        }
+    }
+
+    GetName() {
+        return this.name;
+    }
+
+    SetName(newName) {
+        if (typeof newName === 'string')
+            this.name = newName;
+        return;
+    }
+
+    GetOwner() {
+        return this.teacher;
+    }
+
+    AddCourse(newCourse) {
+        //Have to add a new Node to the List of Courses
+        //const newClass = Classroom();
+
+        //If the input var is a class Course type then append to the list of courses no need to assign the var in the class type
+        //Checking if the data input is class object from Course
+        if(typeof newCourse === 'object')
+            this.courseList.Append(newCourse);
+    }
+
+    RemoveCourse(cName) {
+        var nextHolder = this.courseList.GetFirst().next;
+        if(this.courseList.GetFirst().courseName == cName) {
+            delete(root);
+            root = nextHolder;
+            return;
+        }
+        do {
+            //Checking if the CourseName of the next Node is the node we're looking to remove
+            if(nextHolder.next.courseName == cName) {
+                //Saviing previous node of the CourseName to be removed
+                var prevHolder = nextHolder;
+                nextHolder = nextHolder.next;
+                delete(prevHolder.next);
+                prevHolder.next = nextHolder;
+                return;
+            }
+            nextHolder = nextHolder.next;
+        } while(this.courseList.next != null);
+        //If we get here the CourseName we want to remove is not in the list
+        return;
+    }
+
+    GetCourses() {
+        //
+        return this.courseList;
+    }
+
+    GetClassroomTrend() {
+        return this.classrooomTrend = Trend.GetTable();
+    }
+
+    UpdateClassroomTrend() {
+        this.classrooomTrend = Trend.UpdateTable(this.courseList);
+        return;
+    }
 }
--- Cpts 322 Final Project/Course.js
+++ Cpts 322 Final Project/Course.js
@@ -1,8 +1,83 @@
 
 import Quiz from './Quiz.js'
 import Poll from './Poll.js'
+import LinkedList from './LinkedList.js'
+import RegisteredUser from './RegisteredUser.js'
+import {Teacher} from './Teacher.js'
+import {QuestionSet} from './QuestionSet.js'
 
-class Course {
+
+class Course extends LinkedList, RegisteredUser, Classroom, Teacher {
+
+    constructor() {
+        this.courseName = 'defaultCourseName';
+        this.teacher = null;
+        this.strudents = null;
+        //List<this.Student> null;
+        this.courseTrends = null;
+        this.questionSet = List<QuestionSet>.;
+    }
+
+    TakeAttendance() {
+        if(this.courseName === null)
+            return;
+        Teacher.TakeAttendance(this.courseName);
+        return;   
+    }
+
+    MakeQuestionSet() {
+
+    }
+
+    ModifyQuestionSet() {
+
+    }
+
+    RemoveQuestionSet() {
+
+    }
+
+    GetCourseName() {
+        return this.courseName;
+    }
+
+    GetTeacherName() {
+        if(this.teacher === null)
+            return;
+        return this.teacher;
+    }
+
+    GetStudentNames() {
+        return this.strudents;
+    }
+
+    GetCourseTrend() {
+        
+        return this.courseTrends;
+    }
+
+    GetQuestionSetTrend(qSet) {
+        //have to check if the inputted question set qSet is in the local questionSet linkedList -> List<questionSet>
+        //Call the QuestionSet function for Trend GetQuestiomSetTrend()
+        qSet.GetQuestionSetTrend()
+        return this.questionSet.List<qSet>.GetQuestionSetTrend(QuestionSet* questionList.GetQestionSet());
+    }
 
     
+
+    /*
+        void TakeAttendence();
+        void MakeQuestionSet();
+        Void ModifyQuestionSet();
+        void RemoveQuestionSet();
+        string GetCourseName();
+        string GetTeacherName()
+        List<string> GetStudentName();
+        Trend GetCourseTrend();
+        Trend GetQuestionSetTrend(int);
+        Trend GetQuestionTrend(int, int);
+        void StartQuestionSet();
+        void EndQuestionSet();
+        void UpdateCourseTrend();
+    */
 }
